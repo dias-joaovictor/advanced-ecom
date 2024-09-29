f2f7915ffdf898883537798e416c735bd5ba8d9e
Joao Victor Dias <kvj1610@hotmail.com>
2024-09-29 22:14:25 +0200

    Include Gateway

--- storefront-service/src/main/java/br/com/dias/storefrontservice/client/PaymentServiceClient.java
+++ storefront-service/src/main/java/br/com/dias/storefrontservice/client/PaymentServiceClient.java
@@ -3,7 +3,10 @@ package br.com.dias.storefrontservice.client;
 import br.com.dias.storefrontservice.model.outbound.request.HoldPaymentRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.UUID;
 
@@ -11,8 +14,7 @@ import java.util.UUID;
 @FeignClient("payment-service")
 public interface PaymentServiceClient {
 
-    //    @PostMapping(value = "/payments/hold")
-    @RequestMapping(method = RequestMethod.POST, value = "/payments/hold")
+    @PostMapping(value = "/payments/hold")
     void holdPayment(@RequestBody HoldPaymentRequest holdPaymentRequest);
 
     @PutMapping("/payments/charge/{orderId}")
--- storefront-service/src/main/java/br/com/dias/storefrontservice/model/messaging/OrderProcessed.java
+++ storefront-service/src/main/java/br/com/dias/storefrontservice/model/messaging/OrderProcessed.java
@@ -3,11 +3,13 @@ package br.com.dias.storefrontservice.model.messaging;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class OrderProcessed {
     private UUID orderId;
